0f6150965e1fdf4cd31a18f10f2b57547d83374b
tuyenhp <tuyenhp@runsystem.net>
2026-05-26 20:04:15 +0700

    Init - Seting Hp internal schema

---
+++ structure_data/gmb/666666/schema.js
@@ -0,0 +1,55 @@
+(function () {             const data = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    "name": "tuyenhp_meoshop - ' \" ` \\ / - ( ) [ ] { } < >  | & * ! $",
+    "image": "https://lh3.googleusercontent.com/a-/ALV-UjVDNQd7EFpB9QKXKqZ1TUByRSkO9hVJ1hA9Z6hnFr9HBAxwdm4",
+    "url": "https://en.meo.pt/energy",
+    "telephone": "0393 366 996",
+    "description": "Welcome to MEO SHOP - Pioneering Digital Innovation and AI Solutions.\n\nAt MEO SHOP, we create advanced digital applications and AI-powered platforms. Our mission is to empower businesses with seamless and scalable technology.\n\nDedicated to excellence, our team delivers high-impact solutions that drive results. Let MEO SHOP be your partner in shaping the future with technology !!!",
+    "areaServed": "Da Nang City",
+    "locality": "quận Cẩm Lệ, Đà Nẵng",
+    "address": {
+        "@type": "PostalAddress",
+        "addressCountry": "VN",
+        "postalCode": "550000",
+        "streetAddress": "Tầng 1 Tòa nhà Trực thăng Miền Trung, Đ. Nguyễn Văn Linh, Hòa Thuận Tây, Hải Châu, Đà Nẵng, Vietnam",
+        "addressRegion": "Da Nang City"
+    },
+    "openingHoursSpecification": [
+        {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": "Monday",
+            "opens": "00:00",
+            "closes": "00:00"
+        },
+        {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": "Tuesday",
+            "opens": "00:00",
+            "closes": "00:00"
+        },
+        {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": "Wednesday",
+            "opens": "08:00",
+            "closes": "17:30"
+        },
+        {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": "Thursday",
+            "opens": "08:00",
+            "closes": "17:00"
+        },
+        {
+            "@type": "OpeningHoursSpecification",
+            "dayOfWeek": "Friday",
+            "opens": "08:00",
+            "closes": "11:30"
+        }
+    ],
+    "member": [
+        {
+            "@type": "Person"
+        }
+    ]
+};             var script = document.createElement("script");             script.type = "application/ld+json";             script.text = JSON.stringify(data);             document.head.appendChild(script);           })();
